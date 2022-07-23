5704e36eaf049e89f36ef54f1690faabe630e69c
Lakshmi575 <lakshmi1241@sasi.ac.in>
2022-07-22 20:08:17 -0700

    1st program

---
+++ Basic.java
@@ -0,0 +1,10 @@
+//Program to represent a class 
+class Basic  //Class
+{
+public static void main(String args[])
+{
+String name = "Lakshmi Sai";
+int id = 1241;
+System.out.println("Name is : " + name + " and id is : " + id);
+}
+}
