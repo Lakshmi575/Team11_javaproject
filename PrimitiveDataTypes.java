8c0c96f702ed209a211b09660c7ec5fc0d8ac6a0
Lakshmi575 <lakshmi1241@sasi.ac.in>
2022-07-22 20:06:27 -0700

    Data Types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+//Primitive data types in java
+class PrimitiveDataTypes
+{
+public static void main(String args[])
+{
+byte b=10;
+int i=5;
+float f=25.5f;
+char c='a';
+long l=2500;
+double d=23.99986d;
+boolean o=true;
+System.out.println("byte value is : " +b);
+System.out.println("int value is : " +i);
+System.out.println("float value is : " +f);
+System.out.println("character value is : "+c);
+System.out.println("long value is : " +l);
+System.out.println("double value is : " +d);
+System.out.println("boolean value is : " +o);
+}
+}
+//The primitive data types in java are byte,int,float,char,long,double and boolean.
