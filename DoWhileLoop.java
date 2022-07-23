9b64ecd406a214ff6de02a2358c56b8deb34618e
Lakshmi575 <lakshmi1241@sasi.ac.in>
2022-07-22 20:18:00 -0700

    While Loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,14 @@
+//Program to perform looping statements(DoWhile Loop)
+class DoWhileLoop
+{
+public static void main(String args[])
+{
+int i=1;
+do
+{
+System.out.println("The numbers are : " + i);
+i++;
+}
+while(i<=10);
+}
+}
