9f62ed421767396ab8e82c6f9a8b99a3c40bf5c2
Lakshmi575 <lakshmi1241@sasi.ac.in>
2022-07-22 20:10:24 -0700

    Objects Created

---
+++ ObjectCreation.java
@@ -0,0 +1,19 @@
+//Program to represent objects
+class Main
+{
+int age = 18;
+String name = "Lakshmi";
+void print()
+{
+System.out.println("Name : " + name + " age : " + age);
+}
+}
+class ObjectCreation
+{
+public static void main(String args[])
+{
+Main obj = new Main();
+obj.print();
+}
+}
+//The above program is also an example of object oriented programing.
